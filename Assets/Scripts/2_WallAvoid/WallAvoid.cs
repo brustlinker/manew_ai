4a2d28b5e2c9232a1ee71d505fcd164695d95521
dongshen <1634694819@qq.com>
2016-12-13 20:10:33 +0800

    看完第二章，seekflee

---
+++ Assets/Scripts/2_WallAvoid/WallAvoid.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallAvoid : MonoBehaviour {
+
+	private Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb=GetComponent<Rigidbody2D>();
+		rb.velocity=new Vector3(1,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.DrawLine(transform.position,transform.position+5*getForward(),Color.green);
+		Debug.DrawLine(transform.position,transform.position+5*(getForward()+getRight()),Color.green);
+		Debug.DrawLine(transform.position,transform.position+5*(getForward()+getLeft()),Color.green);
+	}
+
+	/// <summary>
+	/// 获取朝向
+	/// </summary>
+	/// <returns>The forward.</returns>
+	public Vector3  getForward()
+	{
+		Vector2 velocity = rb.velocity;
+		float radians = Mathf.Atan2( velocity.y , velocity.x );
+		return new Vector3(Mathf.Cos(radians), Mathf.Sin(radians),0);
+	}
+
+	public Vector3  getRight()
+	{
+		return -getLeft();
+	}
+
+	public Vector3  getLeft()
+	{
+		Vector2 velocity = rb.velocity;
+		float radians = Mathf.Atan2( velocity.y , velocity.x );
+		return new Vector3(-Mathf.Sin(radians), -Mathf.Cos(radians),0);
+	}
+}
---
+++ Assets/Scripts/3_SeekFlee/Seek.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Seek : MonoBehaviour {
+
+	public Transform target;
+	public float max_velocity;
+	public Vector3 velocity;
+
+	//绘制轨迹
+	private List<Vector3> pathList;
+	private float timer=0;
+
+	// Use this for initialization
+	void Start () {
+		pathList = new List<Vector3>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		//update位置关系
+		Vector3 desiredVelocity = (target.position - transform.position).normalized*max_velocity;
+		Vector3 steeringForce   = desiredVelocity - velocity;
+		Vector3 acc = steeringForce/1;
+		velocity += acc*Time.deltaTime;
+		transform.position+=velocity*Time.deltaTime;
+
+
+
+		//更新绘制路径
+		timer+=Time.deltaTime;
+
+		if(timer>0.1)
+		{
+			timer=0;
+			pathList.Add(transform.position);
+		}
+
+		//绘制
+		for(int i=0;i<pathList.Count-1;i++)
+		{
+			Debug.DrawLine(pathList[i],pathList[i+1],Color.red);
+		}
+
+		//更新朝向
+
+	}
+}
